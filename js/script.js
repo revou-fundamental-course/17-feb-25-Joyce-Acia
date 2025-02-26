335da207821c3d6390703a443cc326d479c5c926
Acia <aciacia06@gmail.com>
2025-02-26 16:57:16 +0700

    17-feb-25

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// ini javascript external
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex == n)
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n> x.length) (slideIndex = 1);
+    if (n < 1) (slideIndex = x.length);
+    for (i = 0; 1 < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
