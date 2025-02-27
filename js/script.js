446d408bc068fcf9c2fcef287827a91bd27179f0
Joyce-Acia <aciacia06@gmail.com>
2025-02-27 21:00:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,52 @@
-// ini javascript external
-var slideIndex = 1;
-showDivs(slideIndex);
+// Ini Javascript External
 
-function plusDivs(n) {
-    showDivs(slideIndex == n)
-}
+function validateForm() {
+    let nameInput = document.getElementById('name-input');
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n> x.length) (slideIndex = 1);
-    if (n < 1) (slideIndex = x.length);
-    for (i = 0; 1 < x.length; i++) {
-        x[i].style.display = "none";
+    // Kondisi Validasinya
+    if (nameInput.value == '') {
+        alert('Please Fill Out Your Name');
+    } else {
+        alert('Welcome ' + nameInput.value);
     }
-    x[slideIndex - 1].style.display = "block";
 }
+
+function validateFormEmail() {
+    let nameInput = document.getElementById('email-input');
+
+    // Kondisi Validasinya
+    if (nameInput.value == '') {
+        alert('Please Fill Out Your Email');
+    } else {
+    }
+}
+// For Index Identifier
+let indexBanner = 0;
+
+showBanner();
+
+function nextBanner() {
+    // Increment IndexBanner
+    indexBanner += 1;
+    showBanner();
+}
+
+function showBanner() {
+    let banner = document.getElementsByClassName('main-banner');
+
+    // Reset Condition
+    if (indexBanner > banner.length - 1) {
+        indexBanner = 0;
+    }
+
+    // Loop untuk hide semua
+    for (let i = 0; i < banner.length; i++) {
+        banner[i].style.display = 'none';
+    }
+
+    // Show Banner
+    banner[indexBanner].style.display = 'block';
+}
+
+// Auto Play
+setInterval(nextBanner, 3000);
